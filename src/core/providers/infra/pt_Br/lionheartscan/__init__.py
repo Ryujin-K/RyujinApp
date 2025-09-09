8c54af78833b257f37cf2ea89ca574365a1c466a
Ryujin-K <kashikoi.ryujin@gmail.com>
2025-09-08 23:09:02 -0300

    update lionheart

--- src/core/providers/infra/pt_Br/lionheartscan/__init__.py
+++ src/core/providers/infra/pt_Br/lionheartscan/__init__.py
@@ -4,38 +4,36 @@ from core.__seedwork.infra.http import Http
 from core.providers.infra.template.base import Base
 from core.providers.domain.entities import Chapter, Pages, Manga
 
-class MeowScannProvider(Base):
-    name = 'Meow Scann'
+class LionHeartScanProvider(Base):
+    name = 'Lion Heart Scan'
     lang = 'pt_Br'
-    domain = ['meowscan1.blogspot.com']
+    domain = ['lionheartscan.xyz']
 
     def __init__(self) -> None:
-        self.API_domain = 'meowscan1.blogspot.com'
-        self.cookie = {'INTERSTITIAL': 'ABqL8_j9iNX3aHhf2VR_tVo_UBu0PZk5TTu29be9X72JDH_DQweTZ4YGL2mMmPk'}
+        self.API = 'https://business-backlion.mu4irv.easypanel.host/manga'
     
     def getManga(self, link: str) -> Manga:
-        response = Http.get(link, cookies=self.cookie)
-        soup = BeautifulSoup(response.content)
-        title = soup.select_one('article strong')
-        
-        return Manga(link, title.get_text(strip=True))
-    
+        id = link.split('/')
+        response = Http.get(f'{self.API}/{id[4]}').json()
+        return Manga(link, response['titulo'])
+
     def getChapters(self, id: str) -> List[Chapter]:
-        response = Http.get(id, cookies=self.cookie)
-        soup = BeautifulSoup(response.content)
-        title = soup.select_one('article strong')
-        response = Http.get(f'https://{self.API_domain}/feeds/posts/default/-/{title.get_text(strip=True)}?alt=json&start-index=1&max-results=150').json()
-        list = []
-        for ch in response['feed']['entry']:
-            if ch['link'][4]['title'] == title.get_text(strip=True):
-                continue
-            list.append(Chapter(ch['link'][4]['href'], ch['title']['$t'], title.get_text(strip=True)))
-        return list
+        id_parts = id.split('/')
+        chapters_data = Http.get(f'{self.API}/{id_parts[4]}/capitulos').json()
+        response = Http.get(f'{self.API}/{id_parts[4]}').json()
+        title = response['titulo']
+        chapters_list = []
+        
+        for chapter in chapters_data:
+            pages = chapter['paginas']
+            chapter_title = chapter['titulo']
+            chapters_list.append(Chapter(pages, chapter_title, title))
+        
+        return chapters_list
+    
     def getPages(self, ch: Chapter) -> Pages:
-        response = Http.get(ch.id)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        get_images = soup.select('div.separator a img')
         list = []
-        for img in get_images:
-            list.append(img.get('src'))
+        for pgs in ch.id:
+            list.append(pgs)
         return Pages(ch.id, ch.number, ch.name, list)
+

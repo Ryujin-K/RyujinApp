87be1aa59515f0390e88f845b48fe71afe646e66
Ryujin <kashikoi.ryujin@gmail.com>
2025-11-09 12:07:53 -0300

    Deprecate EgoToons and NekoToons providers
    Replaced implementation of EgoToonsProvider and NekoToonsProvider with stubs raising NotImplementedError, indicating pending rework after theme deprecation. Removed legacy code and updated base classes to reflect the new structure.

--- src/core/providers/infra/pt_Br/nekotoons/__init__.py
+++ src/core/providers/infra/pt_Br/nekotoons/__init__.py
@@ -1,19 +1,19 @@
-from core.providers.infra.template.yushuke_theme import YushukeTheme
+from core.providers.infra.template.base import Base
 
-class NekoToonsProvider(YushukeTheme):
+
+class NekoToonsProvider(Base):
     name = 'Neko Toons'
     lang = 'pt_Br'
     domain = ['nekotoons.site']
 
     def __init__(self) -> None:
         self.url = 'https://nekotoons.site'
-        self.chapters_api = f'{self.url}/ajax/lzmvke.php?'
-        
-        self.title = 'div.manga-title-row h1'
-        self.manga_id_selector = "button#CarregarCapitulos"
-        self.chapter_item_selector = 'a.chapter-item'
-        self.chapter_number_selector = 'span.capitulo-numero'
-        self.chapter_views_selector = 'span.chapter-views'
-        self.pages_selector = "div.select-nav + * picture"
-        self.id_manga = 'data-manga-id'
-        self.image_selector = "img"
+
+    def getManga(self, link: str):
+        raise NotImplementedError('NekoToonsProvider pending rework after theme deprecation.')
+
+    def getChapters(self, id: str):
+        raise NotImplementedError('NekoToonsProvider pending rework after theme deprecation.')
+
+    def getPages(self, ch):
+        raise NotImplementedError('NekoToonsProvider pending rework after theme deprecation.')
